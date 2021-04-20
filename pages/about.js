efbf54fe8dcf3e2d4568cc17b4779d930e1fcb8e
Tim Williams <tim.90.williams@gmail.com>
2021-04-20 13:40:48 -0500

    Test

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,7 @@ function AboutPage() {
       <h1>About</h1>
       <p>This is an app to find the latest DJ and other musical events</p>
       <p>Version: 1.0.0</p>
+      <h1>Test</h1>
     </div>
   );
 }
